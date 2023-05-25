8a47f82eb01934b90a8c6edba3e1129e8ea363bc
ArshanBhanage <bhanagearshan@gmail.com>
2023-05-25 06:48:00 +0000

    added filters

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,14 @@ class SearchController < ApplicationController
         
         @results = Book.search('*') if params[:search].blank?
         
+        if params[:sort_by] == 'price_high_to_low'
+            @results = @results.order(price: :desc)
+        elsif params[:sort_by] == 'price_low_to_high'
+            @results = @results.order(price: :asc)
+        elsif params[:sort_by].present?
+            @results = @results.where(year: params[:sort_by])
+        end
+        
         render turbo_stream:
             turbo_stream.update('books', partial: 'books/books', locals: {books: @results})
         
