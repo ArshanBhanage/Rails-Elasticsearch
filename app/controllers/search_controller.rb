7103ba7ceb76b5e1493e35d60ec6b41f18c28687
ArshanBhanage <bhanagearshan@gmail.com>
2023-06-01 08:08:16 +0000

    added search and filter together

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,12 @@
 class SearchController < ApplicationController
     def search
-        #args = {}
-        #args[:year] = params[:year] if params[:year].present?
+        
         @results = Book.search(params[:search], page: params[:page], per_page: 5, aggs: {ratings:{}, author:{}, year:{}})
+        
         @ratings_aggregations = @results.aggs['ratings']
         @author_aggregations = @results.aggs['author']
         @year_aggregations = @results.aggs['year']
+        
         @results = Book.search('*', page: params[:page], per_page: 5) if params[:search].blank?
         
         if params[:sort_by] == 'price_high_to_low'
@@ -16,9 +17,6 @@ class SearchController < ApplicationController
         elsif params[:sort_by_year].present?
             @results = @results.where(year: params[:sort_by_year])
         end
-        
-        # price_ranges = [{ to: 100 }, { from: 101, to: 200 }, { from: 201 }]
-        # @price_ranges = Book.search(params[:search], aggs: {price: {ranges: price_ranges}})
         
         render turbo_stream:
             turbo_stream.update('books', partial: 'books/books', locals: { books: @results })
